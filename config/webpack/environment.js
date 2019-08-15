8bebc5409cd11e976c9ab2b744212c935f4e71ad
Alice Fabre <alice.fabre@hotmail.fr>
2019-08-15 12:00:22 +0200

    generate models : cocktail, ingredient, dose

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+// Bootstrap 4 has a dependency over jQuery & Popper.js:
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
 module.exports = environment
