ee0211f34b75122657f702280b818de63e5a0c99
Alice Fabre <alice.fabre@hotmail.fr>
2019-08-15 14:34:50 +0200

    créer 5 ingrédients dans la seed en parsant une url

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# CREER 5 INSTANCES DE INGREDIENT A PARTIR DE L API
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_url_serialized = open(url).read
+ingredient_url = JSON.parse(ingredient_url_serialized)
+
+ingredient_url["drinks"].map! do |ingredient|
+  ingredient["strIngredient1"]
+end
+# p ingredient_url["drinks"]
+
+# ingredient_url_hash = ingredient_url["drinks"]
+# # p ingredient_url_hash
+# ingredients = []
+# ingredient_url_hash.each do |element|
+#   # p element ["strIngredient1"]
+#   ingredients << element ["strIngredient1"]
+# end
+
+puts 'Creating 5 ingredients...'
+5.times do
+  ingredient = Ingredient.new(name: ingredient_url["drinks"].sample)
+  ingredient.save!
+end
+puts 'Finished!'
