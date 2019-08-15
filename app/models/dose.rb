1eae01aaf7ad597a34c93f0b5ec2cd533f2e41e3
Alice Fabre <alice.fabre@hotmail.fr>
2019-08-15 12:48:05 +0200

    ajout des validations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  # A dose must have a description
+  validates :description, presence: true
+  # A dose must have a cocktail and an ingredient, and [cocktail, ingredient] pairings should be unique.
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,5 @@
 class Ingredient < ApplicationRecord
   has_many :doses
+  # An ingredient must have a unique name.
+  validates :name, uniqueness: true, presence: true
 end
