bf1c2e3405a6a5a193b74b08adc9e22e9b98fb04
Alice Fabre <alice.fabre@hotmail.fr>
2019-08-16 17:28:16 +0200

    importer une image via cloudinary et carrier wave

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,7 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   # A cocktail must have a unique name.
   validates :name, uniqueness: true, presence: true
+
+  # uploader des photos
+  mount_uploader :photo, PhotoUploader
 end
